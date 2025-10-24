40488baa7f022257d88b5b2847974b1bd4b505ff
Nadhsb <n-sandb28@hotmail.com>
2025-10-23 23:23:45 -0600

    Agregar código inicial a app.py

---
+++ app.py
@@ -0,0 +1,6 @@
+import streamlit as st
+
+st.header("🚗 Análisis de Datos de Vehículos")
+st.write(
+    "Bienvenido a esta aplicación web interactiva creada con Streamlit. "
+    "Aquí podrás visualizar datos relacionados con vehículos de manera sencilla y dinámica.")
